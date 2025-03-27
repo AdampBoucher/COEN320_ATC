d0a5569aedec24331449b56134bcaa2275423a77
jeffrey Khalife <103140851+jeffreykhalife@users.noreply.github.com>
2025-03-27 00:35:54 -0400

    Update OperatorCommand.cpp
    made the file compatible with c++11

--- src/OperatorCommand.cpp
+++ src/OperatorCommand.cpp
@@ -1,7 +1,7 @@
 #include "OperatorCommand.h"
 #include <sstream>
 
-std::optional<OperatorCommand> OperatorCommandParser::parse(const std::string& input) {
+bool OperatorCommandParser::parse(const std::string& input, OperatorCommand& commandOut) {
     std::istringstream iss(input);
     std::string commandTypeStr;
     std::string id;
@@ -10,17 +10,20 @@ std::optional<OperatorCommand> OperatorCommandParser::parse(const std::string& i
     iss >> commandTypeStr >> id;
 
     if (commandTypeStr == "SET_SPEED") {
-        if (!(iss >> v1 >> v2 >> v3)) return std::nullopt;
-        return OperatorCommand{ CommandType::SET_SPEED, id, v1, v2, v3 };
+        if (!(iss >> v1 >> v2 >> v3)) return false;
+        commandOut = { CommandType::SET_SPEED, id, v1, v2, v3 };
+        return true;
     }
     else if (commandTypeStr == "SET_ALT") {
-        if (!(iss >> v1)) return std::nullopt;
-        return OperatorCommand{ CommandType::SET_ALT, id, 0, 0, v1 }; // Altitude stored in val3
+        if (!(iss >> v1)) return false;
+        commandOut = { CommandType::SET_ALT, id, 0, 0, v1 };
+        return true;
     }
     else if (commandTypeStr == "CHANGE_POS") {
-        if (!(iss >> v1 >> v2 >> v3)) return std::nullopt;
-        return OperatorCommand{ CommandType::CHANGE_POS, id, v1, v2, v3 };
+        if (!(iss >> v1 >> v2 >> v3)) return false;
+        commandOut = { CommandType::CHANGE_POS, id, v1, v2, v3 };
+        return true;
     }
 
-    return std::nullopt;
+    return false;
 }
