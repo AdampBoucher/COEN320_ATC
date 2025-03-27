fc0d890aac86c571f1240537fe49fd3158aa4dd5
jeffrey Khalife <103140851+jeffreykhalife@users.noreply.github.com>
2025-03-27 14:20:53 -0400

    Update OperatorCommand.cpp
    fixed error with makefile

--- src/OperatorCommand.cpp
+++ src/OperatorCommand.cpp
@@ -11,17 +11,29 @@ bool OperatorCommandParser::parse(const std::string& input, OperatorCommand& com
 
     if (commandTypeStr == "SET_SPEED") {
         if (!(iss >> v1 >> v2 >> v3)) return false;
-        commandOut = { CommandType::SET_SPEED, id, v1, v2, v3 };
+        commandOut.type = CommandType::SET_SPEED;
+        commandOut.aircraftId = id;
+        commandOut.val1 = v1;
+        commandOut.val2 = v2;
+        commandOut.val3 = v3;
         return true;
     }
     else if (commandTypeStr == "SET_ALT") {
         if (!(iss >> v1)) return false;
-        commandOut = { CommandType::SET_ALT, id, 0, 0, v1 };
+        commandOut.type = CommandType::SET_ALT;
+        commandOut.aircraftId = id;
+        commandOut.val1 = 0;
+        commandOut.val2 = 0;
+        commandOut.val3 = v1;
         return true;
     }
     else if (commandTypeStr == "CHANGE_POS") {
         if (!(iss >> v1 >> v2 >> v3)) return false;
-        commandOut = { CommandType::CHANGE_POS, id, v1, v2, v3 };
+        commandOut.type = CommandType::CHANGE_POS;
+        commandOut.aircraftId = id;
+        commandOut.val1 = v1;
+        commandOut.val2 = v2;
+        commandOut.val3 = v3;
         return true;
     }
 
